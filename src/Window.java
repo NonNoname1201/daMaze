2fd628560d7437cb4c69477040388e80557d8230
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2023-07-11 20:05:11 +0200

    Rwork on dev: Creation of Window class structure, revork of Main.

--- src/Window.java
+++ src/Window.java
@@ -1,20 +1,32 @@
-import java.awt.GraphicsDevice;
+import java.awt.GraphicsDevice; // To get screen size
 import java.awt.GraphicsEnvironment;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Window {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Full Screen Window");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setUndecorated(true); // remove window decorations
-        frame.setResizable(false); // disable window resizing
+    private Window(String title, int width, int height, Window parent) {
+        JFrame frame = new JFrame(title);
 
-        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice device = env.getDefaultScreenDevice();
-        device.setFullScreenWindow(frame); // set window to fullscreen mode
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        // frame.setUndecorated(true); // remove window decorations
+        frame.setResizable(false); // disable window resizing
 
         frame.setVisible(true);
     }
 
+    public static Window createMainWindow() {
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = env.getDefaultScreenDevice();
+        Dimension screenSize = device.getDefaultConfiguration().getBounds().getSize();
+        int screenWidth = (int) screenSize.getWidth();
+        int screenHeight = (int) screenSize.getHeight();
+        Window mainWindow = new Window("Main Window", screenWidth, screenHeight, null);
+        return mainWindow;
+    }
+
+    public void createSubWindow() {
+        Window subWindow = new Window("Sub Window", 400, 300, this);
+    }
 
 }
